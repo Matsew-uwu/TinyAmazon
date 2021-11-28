76bb1bd4228952042671f275ee8ba657898e0246
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2021-11-29 00:48:01 +0100

    Applying template

--- application/views.py
+++ application/views.py
@@ -1,5 +1,10 @@
 from .app import app
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "<h1>Tiny Amazon</h1>"
+    return render_template(
+        "home.html",
+        title="Tiny Amazon",
+        names = ["LePainDemi", "Oupson", "Sanstepon"]
+    )
