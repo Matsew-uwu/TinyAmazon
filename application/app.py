79bd0063c74813fe2b41e5c495b017cb2cd2009f
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2021-11-29 01:25:59 +0100

    Setting up Bootstrap environement

--- application/app.py
+++ application/app.py
@@ -2,8 +2,16 @@ from os import path
 from posixpath import dirname
 from flask import Flask 
 import yaml, os.path
+from flask_bootstrap import Bootstrap
 
+# Application Set
 app = Flask(__name__)
+
+# Setting up Bootstrap
+Bootstrap(app)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+
 data = yaml.load(
     open(os.path.join(os.path.dirname(__file__), "data.yml")), 
     Loader = yaml.CLoader
