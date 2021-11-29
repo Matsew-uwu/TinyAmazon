e637bfc1381a97eaeda7d46c3c35b5230e2b0dda
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2021-11-29 01:22:48 +0100

    Book sample now displayed

--- application/views.py
+++ application/views.py
@@ -1,10 +1,11 @@
 from .app import app
 from flask import render_template
+from .models import get_sample
 
 @app.route("/")
 def home():
     return render_template(
         "home.html",
         title="Tiny Amazon",
-        names = ["LePainDemi", "Oupson", "Sanstepon"]
+        books = get_sample()
     )
