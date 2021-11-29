8209f9b5b4a39c18b252be22f9c8ef355e23b54a
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2021-11-29 01:06:52 +0100

    Add data.yml to the application

--- application/app.py
+++ application/app.py
@@ -1,3 +1,10 @@
+from os import path
+from posixpath import dirname
 from flask import Flask 
+import yaml, os.path
 
 app = Flask(__name__)
+data = yaml.load(
+    open(os.path.join(os.path.dirname(__file__), "data.yml")), 
+    Loader = yaml.CLoader
+)
